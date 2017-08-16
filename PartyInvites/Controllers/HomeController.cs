5409f94574071a733983de69a4f059a08ef4d483
A.K <adrian.kowalski87@gmail.com>
2017-08-16 23:06:42 +0200

    Added validation for RsvpForm

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PartyInvites.Models;
 
 namespace PartyInvites.Controllers
 {
@@ -16,9 +17,24 @@ namespace PartyInvites.Controllers
             return View();
         }
 
+        [HttpGet]
         public ViewResult RsvpForm()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            //TODO: send guestResponse to party organizer
+
+            if (ModelState.IsValid)
+            {
+                return View("Thanks", guestResponse);
+            }else
+            {
+                return View();
+            }
         }
     }
 }
--- PartyInvites/Models/GuestResponse.cs
+++ PartyInvites/Models/GuestResponse.cs
@@ -2,14 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace PartyInvites.Models
 {
     public class GuestResponse
     {
+        [Required(ErrorMessage = "Please, enter your name and last name.")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Please provide e-mail.")]
+        [RegularExpression(".+\\@.+\\..+,", ErrorMessage = "Please provide valid e-mail.")]
         public string Email { get; set; }
+        [Required(ErrorMessage ="Where is your phone number, ey?")]
         public string Phone { get; set; }
+        [Required(ErrorMessage ="Please make up your mind.")]
         public bool? WillAttend { get; set; }
     }
 }
